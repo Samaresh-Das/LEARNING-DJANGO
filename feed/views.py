8363d54958b348acd5886b92bb3fe33d6e1e93a5
Samaresh Das <samareshmail679@gmail.com>
2022-06-21 21:04:56 +0530

    Finished

--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import TemplateView
+import re
+from django.views.generic import TemplateView, DetailView, FormView 
+from django.contrib import messages
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -9,5 +12,27 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['posts'] = Post.objects.all()
+        context['posts'] = Post.objects.all().order_by('-id') #to order the posts by descending
         return context
+
+class PostDetailView(DetailView):
+    template_name = "detail.html"
+    model = Post
+
+class AddPostView(FormView):
+    template_name = "new_post.html"
+    form_class = PostForm
+    success_url = "/"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        #create a new post
+        new_object = Post.objects.create(
+            text = form.cleaned_data['text'],
+            image = form.cleaned_data['image']
+        )
+        messages.add_message(self.request, messages.SUCCESS, 'Your post was successful')
+        return super().form_valid(form)
